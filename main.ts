c1de7891fc70afb79728acd7bd4731ae1586c0f1
rinkexe <rinkexe@icloud.com>
2026-03-17 09:57:01 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace basic {
     let add = 0
-    function divide(array: number[]) {
+    export function divide(array: number[]) {
         add = 0
         for (let value of array) {
             add += array[value]
