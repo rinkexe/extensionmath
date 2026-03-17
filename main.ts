8695bbbb8b48ae0dfccece01d73e64aa97caa9b0
rinkexe <rinkexe@icloud.com>
2026-03-17 09:53:19 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-let add = 0
-function divide (array: number[]) {
-    add = 0
-    for (let value of array) {
-        add += array[value]
+namespace basic {
+    let add = 0
+    function divide(array: number[]) {
+        add = 0
+        for (let value of array) {
+            add += array[value]
+        }
+        return add / array.length
     }
-    return add / array.length
 }
