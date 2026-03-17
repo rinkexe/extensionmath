6c62a128fe397349009725f0243c50eac8bce42f
rinkexe <rinkexe@icloud.com>
2026-03-17 09:59:01 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace basic {
-    let add = 0
     export function divide(array: number[]) {
+        let add = 0
         add = 0
         for (let value of array) {
             add += array[value]
